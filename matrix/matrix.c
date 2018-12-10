31d4b2c8bfcc330e19f2cfe75fbf18754bef4415
Dipanjan Patra <patraanjan23@gmail.com>
2018-12-11 01:12:24 +0530

    reformatted with clang-format google style added .gitignore

--- matrix/matrix.c
+++ matrix/matrix.c
@@ -1,48 +1,39 @@
+#include "matrix.h"
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
-#include "matrix.h"
 
-/* 
+/*
  * creates a matrix by dynamically allocating memory,
  * mode = 1 => user filled
  * mode = 0 => zero filled
  *
  * returns a matrix
  */
-matrix new_matrix(int row, int col, int mode)
-{
-	matrix tmp = (matrix)malloc(sizeof(float) * row * col);
-	int i, j;
-	float value = 0;
-	for (i = 0; i < row; i++)
-	{
-		for (j = 0; j < col; j++)
-		{
-			if (mode == USER)
-			{
-				printf("[%d][%d] = ", i + 1, j + 1);
-				scanf("%f", &value);
-			}
-			if (mode == UNIT)
-			{
-				if (j == i)
-				{
-					value = 1;
-				}
-				else
-				{
-					value = 0;
-				}
-			}
-			set_element(i, j, col, tmp, value);
-		}
-		if (mode == USER)
-		{
-			printf("\n");
-		}
-	}
-	return tmp;
+matrix new_matrix(int row, int col, int mode) {
+  matrix tmp = (matrix)malloc(sizeof(float) * row * col);
+  int i, j;
+  float value = 0;
+  for (i = 0; i < row; i++) {
+    for (j = 0; j < col; j++) {
+      if (mode == USER) {
+        printf("[%d][%d] = ", i + 1, j + 1);
+        scanf("%f", &value);
+      }
+      if (mode == UNIT) {
+        if (j == i) {
+          value = 1;
+        } else {
+          value = 0;
+        }
+      }
+      set_element(i, j, col, tmp, value);
+    }
+    if (mode == USER) {
+      printf("\n");
+    }
+  }
+  return tmp;
 }
 
 /*
@@ -52,58 +43,48 @@ matrix new_matrix(int row, int col, int mode)
  *	followed by the matrix itself
  *
  *	returns a matrix
-*/
-matrix new_matrix_file(int *row_str_ptr, int *col_str_ptr, char *file_path)
-{
-	FILE *fp = fopen(file_path, "r");
-	if (fp != NULL)
-	{
-		fscanf(fp, "R %d C %d", row_str_ptr, col_str_ptr);
-		int row = *row_str_ptr, col = *col_str_ptr;
-		int i, j;
+ */
+matrix new_matrix_file(int *row_str_ptr, int *col_str_ptr, char *file_path) {
+  FILE *fp = fopen(file_path, "r");
+  if (fp != NULL) {
+    fscanf(fp, "R %d C %d", row_str_ptr, col_str_ptr);
+    int row = *row_str_ptr, col = *col_str_ptr;
+    int i, j;
 
-		matrix tmp = new_matrix(row, col, ZERO);
+    matrix tmp = new_matrix(row, col, ZERO);
 
-		for (i = 0; i < row; i++)
-		{
-			for (j = 0; j < col; j++)
-			{
-				float val;
-				fscanf(fp, "%f", &val);
-				set_element(i, j, col, tmp, val);
-			}
-		}
-		fclose(fp);
-		return tmp;
-	}
-	printf("file %s not found\n", file_path);
-	return NULL;
+    for (i = 0; i < row; i++) {
+      for (j = 0; j < col; j++) {
+        float val;
+        fscanf(fp, "%f", &val);
+        set_element(i, j, col, tmp, val);
+      }
+    }
+    fclose(fp);
+    return tmp;
+  }
+  printf("file %s not found\n", file_path);
+  return NULL;
 }
 
 /*
  * prints a matrix
  * need no of row, col as argument
  */
-void print_matrix(int row, int col, matrix a)
-{
-	int i, j;
-	if (a != NULL)
-	{
-		for (i = 0; i < row; i++)
-		{
-			for (j = 0; j < col; j++)
-			{
-				/* significant digit precision followed */
-				printf("%6.*g ", SIG_DIG, get_element(i, j, col, a));
-			}
-			printf("\n");
-		}
-		printf("\n");
-	}
-	else
-	{
-		printf("\n%s\n", "matrix does not exist");
-	}
+void print_matrix(int row, int col, matrix a) {
+  int i, j;
+  if (a != NULL) {
+    for (i = 0; i < row; i++) {
+      for (j = 0; j < col; j++) {
+        /* significant digit precision followed */
+        printf("%6.*g ", SIG_DIG, get_element(i, j, col, a));
+      }
+      printf("\n");
+    }
+    printf("\n");
+  } else {
+    printf("\n%s\n", "matrix does not exist");
+  }
 }
 
 /*
@@ -112,18 +93,16 @@ void print_matrix(int row, int col, matrix a)
  *
  * returns the float value stored in Matrix[i][j]
  */
-float get_element(int i, int j, int col, matrix x)
-{
-	return *(x + i * col + j);
+float get_element(int i, int j, int col, matrix x) {
+  return *(x + i * col + j);
 }
 
 /*
  * set the value of the element at i-th row and
  * j-th col. requires the no of col of the matrix as arg.
  */
-void set_element(int i, int j, int col, matrix x, float value)
-{
-	*(x + i * col + j) = value;
+void set_element(int i, int j, int col, matrix x, float value) {
+  *(x + i * col + j) = value;
 }
 
 /*
@@ -131,46 +110,40 @@ void set_element(int i, int j, int col, matrix x, float value)
  *
  * returns the sum as a new matrix
  */
-matrix sum(int row, int col, matrix m1, matrix m2)
-{
-	matrix tmp = new_matrix(row, col, ZERO);
-	int i, j;
-	for (i = 0; i < row; i++)
-	{
-		for (j = 0; j < col; j++)
-		{
-			float sum = get_element(i, j, col, m1) + get_element(i, j, col, m2);
-			set_element(i, j, col, tmp, sum);
-		}
-	}
-	return tmp;
+matrix sum(int row, int col, matrix m1, matrix m2) {
+  matrix tmp = new_matrix(row, col, ZERO);
+  int i, j;
+  for (i = 0; i < row; i++) {
+    for (j = 0; j < col; j++) {
+      float sum = get_element(i, j, col, m1) + get_element(i, j, col, m2);
+      set_element(i, j, col, tmp, sum);
+    }
+  }
+  return tmp;
 }
 
 /*
  * an implementation of the above code with dimension checking
  */
-matrix m_sum(int r1, int c1, matrix m1, int r2, int c2, matrix m2)
-{
-	if (r1 != r2 || c1 != c2)
-	{
-		printf("\n%s\n", "operation not possible : dimension mismatch of matrices");
-		return NULL;
-	}
-	return sum(r1, c1, m1, m2);
+matrix m_sum(int r1, int c1, matrix m1, int r2, int c2, matrix m2) {
+  if (r1 != r2 || c1 != c2) {
+    printf("\n%s\n", "operation not possible : dimension mismatch of matrices");
+    return NULL;
+  }
+  return sum(r1, c1, m1, m2);
 }
 
 /*
  * subtraction fn
  */
-matrix m_sub(int r1, int c1, matrix m1, int r2, int c2, matrix m2)
-{
-	matrix _m2 = scalar_product(r2, c2, m2, -1);
-	matrix result = m_sum(r1, c1, m1, r2, c2, _m2);
+matrix m_sub(int r1, int c1, matrix m1, int r2, int c2, matrix m2) {
+  matrix _m2 = scalar_product(r2, c2, m2, -1);
+  matrix result = m_sum(r1, c1, m1, r2, c2, _m2);
 
-	free(_m2);
-	_m2 = NULL;
+  free(_m2);
+  _m2 = NULL;
 
-	return result;
+  return result;
 }
 
 /*
@@ -178,35 +151,31 @@ matrix m_sub(int r1, int c1, matrix m1, int r2, int c2, matrix m2)
  *
  * returns the scalar product as a new matrix
  */
-matrix scalar_product(int row, int col, matrix m, float k)
-{
-	matrix tmp = new_matrix(row, col, ZERO);
-	int i, j;
-	for (i = 0; i < row; i++)
-	{
-		for (j = 0; j < col; j++)
-		{
-			float k_product = k * get_element(i, j, col, m);
-			set_element(i, j, col, tmp, k_product);
-		}
-	}
-	return tmp;
+matrix scalar_product(int row, int col, matrix m, float k) {
+  matrix tmp = new_matrix(row, col, ZERO);
+  int i, j;
+  for (i = 0; i < row; i++) {
+    for (j = 0; j < col; j++) {
+      float k_product = k * get_element(i, j, col, m);
+      set_element(i, j, col, tmp, k_product);
+    }
+  }
+  return tmp;
 }
 
-/* returns transposed matrix so make sure to print the result using transposed dimensions */
-matrix transpose(int row, int col, matrix m)
-{
-	matrix tmp = new_matrix(col, row, ZERO); /* create empty matrix of transpose dim */
-	int i, j;
-	for (i = 0; i < row; i++)
-	{
-		for (j = 0; j < col; j++)
-		{
-			float element = get_element(i, j, col, m);
-			set_element(j, i, row, tmp, element);
-		}
-	}
-	return tmp;
+/* returns transposed matrix so make sure to print the result using transposed
+ * dimensions */
+matrix transpose(int row, int col, matrix m) {
+  matrix tmp =
+      new_matrix(col, row, ZERO); /* create empty matrix of transpose dim */
+  int i, j;
+  for (i = 0; i < row; i++) {
+    for (j = 0; j < col; j++) {
+      float element = get_element(i, j, col, m);
+      set_element(j, i, row, tmp, element);
+    }
+  }
+  return tmp;
 }
 
 /*
@@ -214,66 +183,56 @@ matrix transpose(int row, int col, matrix m)
  *
  * returns the product as a new matrix
  */
-matrix product(int row1, int col1, matrix m1, int row2, int col2, matrix m2)
-{
-	if (col1 != row2)
-	{
-		printf("\n%s\n", "product not possible : dimension mismatch of matrices");
-		return NULL;
-	}
-	matrix tmp = new_matrix(row1, col2, ZERO);
-	int i, j, k;
-	for (i = 0; i < row1; i++)
-	{
-		for (j = 0; j < col2; j++)
-		{
-			float sum = 0;
-			for (k = 0; k < col1; k++)
-			{
-				sum += get_element(i, k, col1, m1) * get_element(k, j, col2, m2);
-			}
-			set_element(i, j, col2, tmp, sum);
-		}
-	}
-	return tmp;
+matrix product(int row1, int col1, matrix m1, int row2, int col2, matrix m2) {
+  if (col1 != row2) {
+    printf("\n%s\n", "product not possible : dimension mismatch of matrices");
+    return NULL;
+  }
+  matrix tmp = new_matrix(row1, col2, ZERO);
+  int i, j, k;
+  for (i = 0; i < row1; i++) {
+    for (j = 0; j < col2; j++) {
+      float sum = 0;
+      for (k = 0; k < col1; k++) {
+        sum += get_element(i, k, col1, m1) * get_element(k, j, col2, m2);
+      }
+      set_element(i, j, col2, tmp, sum);
+    }
+  }
+  return tmp;
 }
 
 /*
  * copy matrix a to matrix b
- * 
+ *
  */
 
-void copy(int row, int col, matrix m1, matrix m2)
-{
-	int i, j;
-	for (i = 0; i < row; i++)
-	{
-		for (j = 0; j < col; j++)
-		{
-			float val = get_element(i, j, col, m1);
-			set_element(i, j, col, m2, val);
-		}
-	}
+void copy(int row, int col, matrix m1, matrix m2) {
+  int i, j;
+  for (i = 0; i < row; i++) {
+    for (j = 0; j < col; j++) {
+      float val = get_element(i, j, col, m1);
+      set_element(i, j, col, m2, val);
+    }
+  }
 }
 
 /*
  * power of a matrix
- * 
+ *
  * returns the power as a new matrix
  */
 
-matrix power(int row, matrix m, int power)
-{
-	matrix tmp = new_matrix(row, row, UNIT);
+matrix power(int row, matrix m, int power) {
+  matrix tmp = new_matrix(row, row, UNIT);
 
-	int p;
-	for (p = 0; p < power; p++)
-	{
-		matrix pow_m = product(row, row, tmp, row, row, m);
-		copy(row, row, pow_m, tmp);
+  int p;
+  for (p = 0; p < power; p++) {
+    matrix pow_m = product(row, row, tmp, row, row, m);
+    copy(row, row, pow_m, tmp);
 
-		free(pow_m);
-		pow_m = NULL;
-	}
-	return tmp;
+    free(pow_m);
+    pow_m = NULL;
+  }
+  return tmp;
 }
