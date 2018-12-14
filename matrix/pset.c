58b34cd3fcb54c9719596a3759f0daeba04f70b9
Dipanjan Patra <patraanjan23@gmail.com>
2018-12-15 02:57:14 +0530

    added new series_test for calculating series sum and fixed all print_matrix calls. removed the redundant printf statements.

--- matrix/pset.c
+++ matrix/pset.c
@@ -8,6 +8,7 @@ void prod_test(int, int, matrix, int, int, matrix);
 void tprod_test(int, int, matrix, int, int, matrix);
 void pow_test(int, int, matrix, int, int, matrix);
 void misc_test(int, int, matrix, int, int, matrix);
+void series_test(int, int, matrix, int, int, matrix);
 
 int main(int argc, char const *argv[]) {
   int r1, c1, r2, c2;
@@ -19,19 +20,17 @@ int main(int argc, char const *argv[]) {
   a = new_matrix_file(&r1, &c1, m_file_1);
   b = new_matrix_file(&r2, &c2, m_file_2);
 
-  printf("matrix a:\n");
-  print_matrix(r1, c1, a);
-  printf("matrix b:\n");
-  print_matrix(r2, c2, b);
+  print_matrix(r1, c1, a, "a");
+  print_matrix(r2, c2, b, "b");
 
   sub_test(r1, c1, a, r2, c2, b);
   prod_test(r1, c1, a, r2, c2, b);
   tprod_test(r1, c1, a, r2, c2, b);
   pow_test(r1, c1, a, r2, c2, b);
   misc_test(r1, c1, a, r2, c2, b);
+  series_test(r1, c1, a, r2, c2, b);
 
-  printf("Press any key to exit\n");
-  getchar();
+  printf("\nPress any key to exit\n");
   getchar();
 
   free(a);
@@ -40,27 +39,23 @@ int main(int argc, char const *argv[]) {
 }
 
 void sub_test(int r1, int c1, matrix m1, int r2, int c2, matrix m2) {
-  printf("result (a - b):\n");
-
   matrix result = m_sub(r1, c1, m1, r2, c2, m2);
-  print_matrix(r1, c1, result);
+  print_matrix(r1, c1, result, "a - b");
 
   free(result);
 }
 
 void prod_test(int r1, int c1, matrix m1, int r2, int c2, matrix m2) {
-  printf("result (a x b):\n");
-
   matrix result = product(r1, c1, m1, r2, c2, m2);
-  print_matrix(r1, c2, result);
+  print_matrix(r1, c2, result, "a x b");
 
-  printf("result (a x cb):\n");
   printf("scalar value c: ");
   float c;
   scanf("%f", &c);
+  getchar();
   matrix cm2 = scalar_product(r2, c2, m2, c);
   matrix result2 = product(r1, c1, m1, r2, c2, cm2);
-  print_matrix(r1, c2, result2);
+  print_matrix(r1, c2, result2, "a x cb");
 
   free(result);
   free(cm2);
@@ -68,29 +63,25 @@ void prod_test(int r1, int c1, matrix m1, int r2, int c2, matrix m2) {
 }
 
 void tprod_test(int r1, int c1, matrix m1, int r2, int c2, matrix m2) {
-  printf("result (a x transpose(b)):\n");
-
   /* dim of transposed matrix */
   int rt = c2, ct = r2;
 
   matrix tm2 = transpose(r2, c2, m2);
   matrix result = product(r1, c1, m1, rt, ct, tm2);
-  print_matrix(rt, ct, result);
+  print_matrix(rt, ct, result, "a x t(b)");
 
   free(tm2);
   free(result);
 }
 
 void pow_test(int r1, int c1, matrix m1, int r2, int c2, matrix m2) {
-  printf("result (a^2 + b^2):\n");
-
   int pwr = 2;
 
   matrix a2 = power(r1, c1, m1, pwr);
   matrix b2 = power(r2, c2, m2, pwr);
 
   matrix result = m_sum(r1, c1, a2, r2, c2, b2);
-  print_matrix(r1, c1, result);
+  print_matrix(r1, c1, result, "a^2 + b^2");
 
   free(a2);
   free(b2);
@@ -98,7 +89,6 @@ void pow_test(int r1, int c1, matrix m1, int r2, int c2, matrix m2) {
 }
 
 void misc_test(int r1, int c1, matrix m1, int r2, int c2, matrix m2) {
-  printf("result (a x b) - (b x a) & (a x b) + (b x a):\n");
   int r_axb = r1, c_axb = c2;
   int r_bxa = r2, c_bxa = c1;
 
@@ -108,11 +98,36 @@ void misc_test(int r1, int c1, matrix m1, int r2, int c2, matrix m2) {
   matrix result1 = m_sub(r_axb, c_axb, axb, r_bxa, c_bxa, bxa);
   matrix result2 = m_sum(r_axb, c_axb, axb, r_bxa, c_bxa, bxa);
 
-  print_matrix(r_axb, c_axb, result1);
-  print_matrix(r_axb, c_axb, result2);
+  print_matrix(r_axb, c_axb, result1, "a x b - b x a");
+  print_matrix(r_axb, c_axb, result2, "a x b + b x a");
 
   free(axb);
   free(bxa);
   free(result1);
   free(result2);
+}
+
+void series_test(int r1, int c1, matrix m1, int r2, int c2, matrix m2) {
+
+  /* sum = sum + term^n */
+
+  int n;
+  printf("integer n: ");
+  scanf("%d", &n);
+  getchar(); /* absorbs the following \n */
+
+  matrix result = new_matrix(r1, c1, ZERO);
+  int i;
+  for (i = 0; i < n; i++) {
+    matrix nterm = power(r1, c1, m1, i);
+    matrix tmp_sum = m_sum(r1, c1, nterm, r1, c1, result);
+    copy(r1, c1, tmp_sum, result);
+
+    free(nterm);
+    free(tmp_sum);
+    nterm = NULL;
+    tmp_sum = NULL;
+  }
+
+  print_matrix(r1, c1, result, "1 + a + a^2 + a^3 + ... + a^n");
 }
